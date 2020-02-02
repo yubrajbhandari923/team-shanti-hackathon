417ab8646ca4c89861fc1a2303e2b390eff2de91
yubrajbhandari923 <yubrajbhandari32@yahoo.com>
2020-02-02 19:47:54 +0545

    minor

--- user_dashboard.php
+++ user_dashboard.php
@@ -13,25 +13,14 @@
     <link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="refrences/css/userdashboard.css">
-    <link rel="stylesheet" href="refrences/css/header.css">
     <script src='refrences/js/jquery.js'></script>
     <script src='refrences/js/fetchUser.js'></script>
     <title>Login</title>
 </head>
 <body>
     <section class="bodyContainer">
-        <header class="sideBar">
-            <nav>
-                <ul>
-                    <li class="headerItem headerItem--active">
-                        <i class="fas fa-user"></i>
-                    </li>
-                    <li class="headerItem"><i class="fas fa-bell"></i></li>
-                    <a id='logout'><li class="headerItem">C</li></a>
-                    <li class="headerItem headerItem--logOut"><i class="fas fa-sign-out-alt"></i></li>      
-                </ul>
-            </nav>
-        </header>
+        <?php include "header.php" ?>
+
         <section class="bodyContent">
             <div class="contentHead">
                 <h1 >Dashboard</h1>
