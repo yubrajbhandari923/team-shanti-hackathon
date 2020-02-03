aa900b3cb2b9e0b6da62ec6368b57c194bd74ac1
anup8egy <anup8eguy@gmail.com>
2020-02-03 07:30:52 +0545

    search page completed fend and back end

---
+++ handle/removeCards.php
@@ -0,0 +1,15 @@
+<?php
+    define('session-cookie_check',TRUE);
+    include 'session-cookie_check.php';
+    if(!isset($_SERVER['HTTP_REFERER'])){
+        include 'error.php';
+        exit;
+    }
+        define('sql-connection_check',TRUE);
+        define('check',TRUE);
+        include 'session-cookie_check.php';
+        include 'sql-connection.php';
+        include '../crypt.php';
+        $currUserId='2';
+        $currheadText='Coer';
+        $picLoc='stg';
